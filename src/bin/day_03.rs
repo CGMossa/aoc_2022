f3fe1ffc99d1b241cd103bf7190aae6e18e49f1d
Mossa <cgmossa@gmail.com>
2022-12-03 21:37:45 +0100

    day 3 two star

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -3,6 +3,43 @@ use std::collections::HashSet;
 use itertools::Itertools;
 
 fn main() {
+    one_star();
+    two_star();
+}
+
+fn two_star() {
+    let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw";
+    let input = include_str!("../../input/day03.txt");
+    let inventory = input
+        .lines()
+        // remove whitespace from the lines
+        .map(|x| x.chars().filter(|x| !x.is_whitespace()))
+        .map(|x| x.collect::<Vec<_>>())
+        // groups
+        .collect_vec();
+    let priority = inventory
+        .chunks(3)
+        .flat_map(|chunk| {
+            let (x, y, z) = chunk.into_iter().collect_tuple().unwrap();
+            x.into_iter()
+                .filter(|x| y.contains(x) && z.contains(x))
+                .next()
+                .copied()
+        })
+        .map(|x| x as u8)
+        .map(|x| x - if x >= 97 { 96 } else { 38 })
+        .map(|x| -> u32 { x.try_into().unwrap() })
+        // .inspect(|x| println!("{}", x))
+        ;
+    dbg!(priority.sum::<u32>());
+}
+
+fn one_star() {
     let input = "vJrwpWtwJgWrhcsFMMfFFhFp
     jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
     PmmdzqPrVvPwwTWBwg
@@ -19,24 +56,20 @@ fn main() {
                 .iter()
                 .filter(|x| second.contains(x))
                 .copied()
-                //FIXME: find another way to remove duplicates
-                .collect::<HashSet<_>>()
+                .next()
         });
     let priority = rucksacks
         // .inspect(|x| println!("{}", x))
-        // 65..91 for upper letters and 97..123 for lower letters
-        // ~>
-        // 1..27  for lower letters and 27..53 for upper letters
-        //
-        // lower: 97..123 -> 1..27  x |-> x - 96
-        // upper: 65..91  -> 27..53  x |-> x - 38
+        // lower: 97..123 -> 1..27,  x |-> x - 96
+        // upper: 65..91  -> 27..53, x |-> x - 38
         //
         .map(|x| x as u8)
         .map(|x| x - if x >= 97 { 96 } else { 38 })
         .map(|x| -> u32 { x.try_into().unwrap() })
-        .inspect(|x| println!("{}", x));
+        // .inspect(|x| println!("{}", x));
+        ;
+    dbg!(priority.sum::<u32>());
     // dbg!(rucksacks.clone().collect::<String>());
     // dbg!((priority).collect::<Vec<_>>());
-    dbg!(priority.sum::<u32>());
     // .inspect(|x| println!("{}", x)).collect_vec();
 }
