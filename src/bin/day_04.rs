37228e2726fd2a6b20f0ad01447235035a9b268a
Mossa <cgmossa@gmail.com>
2022-12-04 09:36:36 +0100

    day 4 one star

---
+++ src/bin/day_04.rs
@@ -0,0 +1,32 @@
+use itertools::Itertools;
+
+fn main() {
+    one_star();
+}
+
+fn one_star() {
+    let input = include_str!("../../input/day04.txt");
+    let answer = input
+        .lines()
+        .map(|xy| -> (u32, u32, u32, u32) {
+            xy.split(',')
+                .map(|xy| xy.split('-').map(|x| x.parse::<u32>().unwrap()))
+                .flatten()
+                .collect_tuple()
+                .unwrap()
+        })
+        // .inspect(|xy| println!("{:?}", xy))
+        .fold(0, |acc, abxy| {
+            let (a, b, x, y) = abxy;
+            /* a-b <= x-y */
+            /* x-y <= a-b */
+            if (x <= a && b <= y) || (a <= x && y <= b)
+            {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+    // .collect_vec();
+    println!("Answer: {}", answer);
+}
