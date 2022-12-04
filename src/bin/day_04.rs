b85e8960d89d68144ff62cf5c99518bf2d3fb0c4
Mossa <cgmossa@gmail.com>
2022-12-04 09:41:50 +0100

    day 4 two star

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -2,6 +2,38 @@ use itertools::Itertools;
 
 fn main() {
     one_star();
+    two_star();
+}
+
+fn two_star() {
+    let input = include_str!("../../input/day04.txt");
+    let answer = input
+        .lines()
+        .map(|xy| -> (u32, u32, u32, u32) {
+            xy.split(',')
+                .map(|xy| xy.split('-').map(|x| x.parse::<u32>().unwrap()))
+                .flatten()
+                .collect_tuple()
+                .unwrap()
+        })
+        // .inspect(|xy| println!("{:?}", xy))
+        .fold(0, |acc, abxy| {
+            let (a, b, x, y) = abxy;
+            /* a-b <= x-y */
+            /* x-y <= a-b */
+            // if (x <= a && b <= y) || (a <= x && y <= b)
+            if ((x <= a) & (a <= y))
+                | ((x <= b) & (b <= y))
+                | ((a <= x) & (x <= b))
+                | ((a <= y) & (y <= b))
+            {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+    // .collect_vec();
+    println!("Answer: {}", answer);
 }
 
 fn one_star() {
@@ -20,8 +52,7 @@ fn one_star() {
             let (a, b, x, y) = abxy;
             /* a-b <= x-y */
             /* x-y <= a-b */
-            if (x <= a && b <= y) || (a <= x && y <= b)
-            {
+            if (x <= a && b <= y) || (a <= x && y <= b) {
                 acc + 1
             } else {
                 acc
