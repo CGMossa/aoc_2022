e0514c2c0912833f393420d332ba7c2695cf3c10
Mossa <cgmossa@gmail.com>
2022-12-05 18:27:29 +0100

    day 05 two star

--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -1,4 +1,4 @@
-use std::{array, collections::VecDeque};
+use std::collections::VecDeque;
 
 use itertools::Itertools;
 
@@ -33,6 +33,7 @@ impl MoveInstruction {
 
 fn main() {
     one_star();
+    two_star();
 }
 
 fn one_star() {
@@ -51,7 +52,7 @@ move 1 from 1 to 2";
     // three spaces for each position, and separated by one space.
     let first_line_length = input.chars().position(|x| x == '\n').unwrap();
     let stacks = first_line_length / 4 + if first_line_length % 4 == 0 { 0 } else { 1 };
-    dbg!(first_line_length, stacks);
+    // dbg!(first_line_length, stacks);
 
     let (initial_state_input, instructions_input) = input.split("\n\n").collect_tuple().unwrap();
     // parse initial state of stacks
@@ -65,7 +66,7 @@ move 1 from 1 to 2";
             initial_state.inner[id].push_back(c);
         }
     });
-    println!("{:#?}", initial_state);
+    // println!("{:#?}", initial_state);
 
     // parse instructions
     let instructions = instructions_input.lines().map(|x| {
@@ -92,7 +93,72 @@ move 1 from 1 to 2";
             current_stacks.inner[to].push_front(ele);
         }
     }
-    println!("{:#?}", current_stacks);
+    // println!("{:#?}", current_stacks);
+    let answer = current_stacks
+        .inner
+        .into_iter()
+        .flat_map(|mut x| x.pop_front())
+        .join("");
+    println!("Answer: {}", answer);
+}
+
+fn two_star() {
+    let input = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    let input = include_str!("../../input/day_05.txt");
+
+    // three spaces for each position, and separated by one space.
+    let first_line_length = input.chars().position(|x| x == '\n').unwrap();
+    let stacks = first_line_length / 4 + if first_line_length % 4 == 0 { 0 } else { 1 };
+    // dbg!(first_line_length, stacks);
+
+    let (initial_state_input, instructions_input) = input.split("\n\n").collect_tuple().unwrap();
+    // parse initial state of stacks
+    let mut initial_state: Stacks = Stacks::new(stacks);
+    initial_state_input.lines().for_each(|line| {
+        for (id, c) in line.char_indices() {
+            if !matches!(c, 'A'..='Z') {
+                continue;
+            }
+            let id = id / 4;
+            initial_state.inner[id].push_back(c);
+        }
+    });
+    // println!("Initial state:\n\t{:#?}", initial_state);
+
+    // parse instructions
+    let instructions = instructions_input.lines().map(|x| {
+        let x = x.replace("move ", "");
+        let (count, rest) = x.split(" from ").collect_tuple().unwrap();
+        let count = count.parse().unwrap();
+        let (from, to) = rest.split(" to ").collect_tuple().unwrap();
+        let from: usize = from.parse().unwrap();
+        let to: usize = to.parse().unwrap();
+
+        let from = from - 1;
+        let to = to - 1;
+
+        MoveInstruction { count, from, to }
+    });
+
+    let mut current_stacks = initial_state;
+    for instr in instructions {
+        let MoveInstruction { count, from, to } = instr;
+        let elements = current_stacks.inner[from].drain(0..count).collect_vec();
+        elements
+            .into_iter()
+            .rev()
+            .for_each(|x| current_stacks.inner[to].push_front(x));
+    }
+    // println!("Last state:\n\t{:#?}", current_stacks);
     let answer = current_stacks
         .inner
         .into_iter()
