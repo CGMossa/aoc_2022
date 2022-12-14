399440f05be1707ca7fd81fe3f8f79805a16f340
Mossa <cgmossa@gmail.com>
2022-12-14 18:02:28 +0100

    day 09 two star..

--- src/bin/day_09.rs
+++ src/bin/day_09.rs
@@ -20,9 +20,24 @@ R 2"
     );
     let input = include_str!("../../input/day_09.txt");
     println!("Answer: {}", one_star(input));
+
+    assert_eq!(
+        two_star(
+            "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
+        ),
+        36
+    );
+    println!("Answer: {}", two_star(input));
 }
 
-#[derive(Debug, Clone, derive_more::Display)]
+#[derive(Debug, Clone, Copy, derive_more::Display)]
 enum Direction {
     #[display(fmt = "→")]
     Right,
@@ -42,7 +57,7 @@ enum Direction {
     DownRight,
 }
 
-#[derive(Debug, Clone, Copy, derive_more::Display)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, derive_more::Display)]
 #[display(fmt = "{:?}", "inner")]
 struct Position {
     inner: (isize, isize),
@@ -125,7 +140,7 @@ fn one_star(input: &str) -> usize {
     for (direction, steps) in instructions {
         // println!("== {direction} {steps} ==");
         for _step in 0..steps {
-            head.apply_instruction_once(direction.clone());
+            head.apply_instruction_once(direction);
             let is_adjacent = [-1, 0, 1]
                 .iter()
                 .cartesian_product([-1, 0, 1].iter())
@@ -146,28 +161,67 @@ fn one_star(input: &str) -> usize {
             visited.insert(tail.inner);
 
             // check if the head and tail are adjacent after the update
-            assert!([-1, 0, 1]
-                .iter()
-                .cartesian_product([-1, 0, 1].iter())
-                .any(|x| {
-                    (tail.inner.0 + x.0 == head.inner.0) && (tail.inner.1 + x.1 == head.inner.1)
-                }));
+            assert!(check_tail_within(head, tail));
         }
         // // println!("== {direction} {steps} ==");
         // display_state(5, 6, start.clone(), head.clone(), tail.clone());
         // println!();
     }
 
-    // display_visited(5, 6, start, visited.iter().collect_vec().as_slice());
+    visited.len()
+}
 
-    // dbg!(
-    //     visited.iter().max_by_key(|x| -x.0),
-    //     visited.iter().max_by_key(|x| x.0),
-    //     visited.iter().max_by_key(|x| -x.1),
-    //     visited.iter().max_by_key(|x| x.1)
-    // );
+fn two_star(input: &str) -> usize {
+    let instructions = input
+        .lines()
+        .map(|x| {
+            let (direction, steps) = x.split_once(' ').unwrap();
+            let steps: isize = steps.parse().unwrap();
+            let direction: Direction = match direction {
+                "U" => Direction::Up,
+                "L" => Direction::Left,
+                "D" => Direction::Down,
+                "R" => Direction::Right,
+                _ => panic!("provided instruction `{}` doesn't exist", direction),
+            };
+            (direction, steps)
+        })
+        .collect_vec();
+    let mut visited: HashSet<Position> = Default::default();
+
+    let start = Position::new(0, 0);
+    // [H, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let mut rope = [start; 10];
+    visited.insert(start);
+    for (direction, steps) in instructions {
+        for _step in 0..steps {
+            rope[0].apply_instruction_once(direction);
+            for id in 0..rope.len() - 1 {
+                let head = rope[id];
+                let tail = &mut rope[id + 1];
+                let is_adjacent = check_tail_within(head, *tail);
+                if is_adjacent {
+                    continue;
+                }
+                let direct = (head.inner.0 - tail.inner.0, head.inner.1 - tail.inner.1);
+                let delta = (direct.0.signum(), direct.1.signum());
+                tail.inner.0 += delta.0;
+                tail.inner.1 += delta.1;
+            }
+            let true_tail = *rope.last().unwrap();
+            visited.insert(true_tail);
+        }
+    }
 
     visited.len()
+}
+
+fn check_tail_within(head: Position, tail: Position) -> bool {
+    // check if the head and tail are adjacent after the update
+    [-1, 0, 1]
+        .iter()
+        .cartesian_product([-1, 0, 1].iter())
+        .any(|x| (tail.inner.0 + x.0 == head.inner.0) && (tail.inner.1 + x.1 == head.inner.1))
 }
 
 fn display_visited(rows: usize, cols: usize, start: Position, visited: &[&(isize, isize)]) {
